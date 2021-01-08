fecab82d522960ef92ea8c0f458065098065bbab
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2021-01-08 14:21:02 +0100

    Aggregated file now moved to -history-stg bucket. DAT-5302

--- scripts/aggregate_backup_files.py
+++ scripts/aggregate_backup_files.py
@@ -6,7 +6,10 @@ import tarfile
 import gzip
 
 from datetime import datetime, timedelta
-from google.cloud import storage, exceptions
+from google.cloud import storage, exceptions as gcp_exceptions
+from retry import retry
+
+logging.getLogger().setLevel(logging.INFO)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--data-catalog', required=True)
@@ -21,24 +24,35 @@ date = datetime.now() - timedelta(2)
 
 class GCSBucketProcessor:
     def __init__(self, client, bucket_name_prefix, process_date):
+        """
+        Initializes GCS Bucket processor.
+        """
+
         self.client = client
 
         self.process_date = process_date
+        self.bucket_name_prefix = bucket_name_prefix
         self.staging_bucket_name = f"{bucket_name_prefix}-hst-sa-stg"
         self.backup_bucket_name = f"{bucket_name_prefix}-history-stg"
         self.bucket_prefix = datetime.strftime(process_date, '%Y/%m/%d')
 
         self.aggregated_file_name = f"{self.staging_bucket_name}_{datetime.strftime(process_date, '%Y%m%d')}.tar.xz"
 
-        self.bucket = None
+        self.staging_bucket = None
+        self.backup_bucket = None
 
     def aggregate_blobs(self):
-        try:
-            self.bucket = self.client.get_bucket(self.staging_bucket_name)
-        except exceptions.NotFound:
-            raise SystemError(f"Bucket '{self.staging_bucket_name}' does not exist, skipping aggregation")
+        """
+        Aggregates blobs within class bucket.
+        """
 
-        bucket_blobs = list(self.bucket.list_blobs(prefix=self.bucket_prefix))
+        try:  # Connect to staging (hst-sa-stg) and backup (history-stg) buckets
+            self.staging_bucket = self.client.get_bucket(self.staging_bucket_name)
+            self.backup_bucket = self.client.get_bucket(self.backup_bucket_name)
+        except gcp_exceptions.NotFound as e:
+            raise SystemError(f"{str(e)}, skipping aggregation")
+
+        bucket_blobs = list(self.staging_bucket.list_blobs(prefix=self.bucket_prefix))
 
         if len(bucket_blobs) == 0:
             return
@@ -48,25 +62,29 @@ class GCSBucketProcessor:
 
             with tarfile.open(fileobj=temp_file, mode='w:xz') as tar:
                 for blob in bucket_blobs:
+                    # Skip possible previous created aggregation file
                     if f"{self.bucket_prefix}/{self.aggregated_file_name}" == blob.name:
                         continue
 
-                    blob_data, blob_size, blob_name = self.get_blob_data(blob)  # Get parsed blob data
+                    # Get parsed blob data
+                    blob_data, blob_size, blob_name = self.get_blob_data(blob)
 
+                    # Add file and info to tar
                     info = tarfile.TarInfo(blob_name)
                     info.size = blob_size
                     tar.addfile(info, blob_data)
         except Exception as e:
             logging.exception(e)
         else:
-            self.save_aggregated_file(temp_file)
-            temp_file.close()
-
-            # TODO: Move aggregated file
-            # TODO: Delete processed files
+            self.process_aggregated_file(temp_file)
 
     @staticmethod
+    @retry(tries=3, delay=2, backoff=2)
     def get_blob_data(blob):
+        """
+        Retrieves blob data from GCS bucket, does parse .gz files first.
+        """
+
         temp_file = tempfile.TemporaryFile(mode='w+b')
         blob.download_to_file(temp_file)
         temp_size = temp_file.tell()
@@ -84,14 +102,65 @@ class GCSBucketProcessor:
 
         return temp_file, temp_size, blob.name
 
-    def save_aggregated_file(self, temp_file):
-        logging.info(f"Uploading file '{self.aggregated_file_name}' with size '{temp_file.tell()}'")
+    def process_aggregated_file(self, temp_file):
+        """
+        Processes created aggregated file towards correct buckets.
+        """
 
-        blob = self.bucket.blob(f"{self.bucket_prefix}/{self.aggregated_file_name}")
-        blob.upload_from_file(temp_file, rewind=True, content_type="application/x-xz")
+        try:
+            staging_blob = self.save_aggregated_file(temp_file)
+            history_file_exists = self.move_aggregated_file(staging_blob)
+        except Exception:
+            raise
+        else:
+            # TODO: Delete processed files
+
+            return history_file_exists
+
+    @retry(tries=3, delay=2, backoff=2)
+    def save_aggregated_file(self, temp_file):
+        """
+        Saves aggregated file towards staging bucket (-hst-sa-stg).
+        """
+
+        blob_name = f"{self.bucket_prefix}/{self.aggregated_file_name}"
+        blob_location = f"gs://{self.staging_bucket_name}/{blob_name}"
+        logging.info(f"Uploading aggregated file to '{blob_location}'")
+
+        try:
+            blob = self.staging_bucket.blob(blob_name)
+            blob.upload_from_file(temp_file, rewind=True, content_type="application/x-xz")
+        except Exception as e:
+            logging.error(f"Something went wrong during file upload: {str(e)}")
+            raise
+        else:
+            temp_file.close()
+            return blob
+
+    @retry(tries=3, delay=2, backoff=2)
+    def move_aggregated_file(self, staging_blob):
+        """
+        Copies aggregated file towards history bucket (-history-stg).
+        """
+
+        blob_name = f"{self.bucket_prefix}/{self.aggregated_file_name}"
+        blob_location = f"gs://{self.backup_bucket_name}/{blob_name}"
+        logging.info(f"Copying aggregated file to '{blob_location}'")
+
+        try:
+            self.staging_bucket.copy(staging_blob, self.backup_bucket, new_name=blob_name)
+        except Exception as e:
+            logging.error(f"Something went wrong during file copy: {str(e)}")
+            raise
+        else:
+            return storage.Blob(bucket=self.backup_bucket, name=blob_name).exists(self.client)
 
 
 def get_catalog_topic_names():
+    """
+    Retrieves Pub/Sub Topic names from passed data-catalog.
+    """
+
     topic_names = []
 
     catalog_file = open(data_catalog, "r")
@@ -107,16 +176,23 @@ def get_catalog_topic_names():
 
 
 def aggregate_backup_files():
+    """
+    Retrieves list of topics and aggregates all blobs.
+    """
+
     topic_names = get_catalog_topic_names()
 
     logging.info(f"Found '{len(topic_names)}' topics to aggregate")
 
     for topic in topic_names:
         try:
+            logging.info(f"Starting aggregation for topic '{topic}'")
             GCSBucketProcessor(client=stg_client, bucket_name_prefix=topic, process_date=date).aggregate_blobs()
         except Exception as e:
-            logging.error(str(e))
+            logging.error(f"Failed aggregation for topic '{topic}': {str(e)}")
             continue
+        else:
+            logging.info(f"Finished aggregation for topic '{topic}'")
 
 
 aggregate_backup_files()
