29ccd097cfb2980af67476d5d5d4a2502f47e2cc
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2021-01-11 14:41:21 +0100

    Changed Pub/Sub logging level to WARNING. DAT-5314

--- functions/mirror-to-azure/main.py
+++ functions/mirror-to-azure/main.py
@@ -8,6 +8,7 @@ from concurrent.futures import TimeoutError
 from azure.eventhub import EventHubProducerClient, EventData
 
 logging.basicConfig(level=logging.INFO)
+logging.getLogger('google.cloud.pubsub_v1').setLevel(logging.WARNING)
 
 producer = None
 subscription_path = None
