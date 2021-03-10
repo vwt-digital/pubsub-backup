fde8d2dd60aa785b318736cdcf4c22d331e9e705
Eric van der Velde <e.vandervelde@vwt.digital>
2021-03-10 09:58:31 +0100

    Added 'await_msg_callbacks' flag. DAT-5983

--- functions/pubsub-backup/main.py
+++ functions/pubsub-backup/main.py
@@ -1,23 +1,21 @@
+import json
+import logging
 import os
 import sys
-import json
-import time
-import logging
-import uuid
 import threading
-
-from google.cloud import storage
-from google.cloud import pubsub_v1
+import time
+import uuid
 from datetime import datetime
 
+from google.cloud import pubsub_v1, storage
 from retry import retry
 
 logging.basicConfig(level=logging.INFO)
-logging.getLogger('google.cloud.pubsub_v1').setLevel(logging.WARNING)
-logging.getLogger('google.api_core').setLevel(logging.WARNING)
+logging.getLogger("google.cloud.pubsub_v1").setLevel(logging.WARNING)
+logging.getLogger("google.api_core").setLevel(logging.WARNING)
 
-PROJECT_ID = os.getenv('PROJECT_ID')
-FUNCTION_TIMEOUT = int(os.getenv('FUNCTION_TIMEOUT', '500'))
+PROJECT_ID = os.getenv("PROJECT_ID")
+FUNCTION_TIMEOUT = int(os.getenv("FUNCTION_TIMEOUT", "500"))
 
 
 def handler(request):
@@ -25,15 +23,15 @@ def handler(request):
     ps_client = pubsub_v1.SubscriberClient()
 
     try:
-        subscription = request.data.decode('utf-8')
+        subscription = request.data.decode("utf-8")
         subscription_path = ps_client.subscription_path(PROJECT_ID, subscription)
         logging.info(f"Starting to archive messages from {subscription_path}...")
         pull(subscription, subscription_path, ps_client)
     except Exception as e:
         logging.exception(f"Something bad happened, reason: {e}")
-        return 'ERROR', 501
+        return "ERROR", 501
 
-    return 'OK', 204
+    return "OK", 204
 
 
 @retry(ConnectionError, tries=3, delay=5, backoff=2, logger=None)
@@ -47,26 +45,28 @@ def to_storage(messages, bucket_name, prefix, epoch, unique_id):
 
 
 def subscription_to_bucket(subscription):
-    return subscription.replace('-history-sub', '-hst-sa-stg')
+    return subscription.replace("-history-sub", "-hst-sa-stg")
 
 
 def chunk(lst, n):
     for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+        yield lst[i : i + n]  # noqa: E203
 
 
 def write_to_file(subscription, messages):
     unique_id = str(uuid.uuid4())[:16]
     now = datetime.now()
     epoch = int(now.timestamp())
-    prefix = now.strftime('%Y/%m/%d/%H')
+    prefix = now.strftime("%Y/%m/%d/%H")
     bucket_name = subscription_to_bucket(subscription)
 
     try:
         to_storage(messages, bucket_name, prefix, epoch, unique_id)
     except Exception as e:
-        logging.exception(f"Storing of file in gs://{bucket_name}/{prefix} failed, reason: {e}")
-        return 'ERROR', 501
+        logging.exception(
+            f"Storing of file in gs://{bucket_name}/{prefix} failed, reason: {e}"
+        )
+        return "ERROR", 501
 
 
 def ack(subscription_path, ack_ids, ps_client):
@@ -74,14 +74,12 @@ def ack(subscription_path, ack_ids, ps_client):
         chunks = chunk(ack_ids, 1000)
         for batch in chunks:
             ps_client.acknowledge(
-                request={
-                    "subscription": subscription_path,
-                    "ack_ids": batch
-                })
+                request={"subscription": subscription_path, "ack_ids": batch}
+            )
         logging.info(f"Acknowledged {len(ack_ids)} message(s) from {subscription_path}")
     except Exception as e:
         logging.exception(f"Acknowleding failed, reason: {e}")
-        return 'ERROR', 501
+        return "ERROR", 501
 
 
 def pull(subscription, subscription_path, ps_client):
@@ -110,9 +108,10 @@ def pull(subscription, subscription_path, ps_client):
             ack(subscription_path, ack_ids, ps_client)
 
     streaming_pull_future = ps_client.subscribe(
-                        subscription_path,
-                        callback=callback,
-                        flow_control=pubsub_v1.types.FlowControl(max_messages=75000))
+        subscription_path,
+        callback=callback,
+        flow_control=pubsub_v1.types.FlowControl(max_messages=75000),
+    )
 
     streaming_pull_future.add_done_callback(done_callback)
 
@@ -125,13 +124,13 @@ def pull(subscription, subscription_path, ps_client):
     try:
         while True:
             # Less than 25 messages stop collecting
-            if len(messages)-last_nr_messages < 25:
-                streaming_pull_future.cancel()
+            if len(messages) - last_nr_messages < 25:
+                streaming_pull_future.cancel(await_msg_callbacks=True)
                 break
 
             # limit the duration of the function
             if (datetime.now() - start).total_seconds() > FUNCTION_TIMEOUT:
-                streaming_pull_future.cancel()
+                streaming_pull_future.cancel(await_msg_callbacks=True)
                 break
 
             if sys.getsizeof(json.dumps(messages)) > 2500000:
@@ -155,11 +154,13 @@ def pull(subscription, subscription_path, ps_client):
             time.sleep(1)
 
     except TimeoutError:
-        streaming_pull_future.cancel()
+        streaming_pull_future.cancelawait_msg_callbacks = True()
     except Exception:
-        logging.exception(f"Listening for messages on {subscription_path} threw an exception.")
+        logging.exception(
+            f"Listening for messages on {subscription_path} threw an exception."
+        )
 
     # Wait until the whole Future is done
     while not streaming_pull_future.done():
-        logging.info('=== WAIT ===')
+        logging.info("=== WAIT ===")
         time.sleep(0.1)
